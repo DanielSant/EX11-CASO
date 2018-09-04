195922cc5c14af7252dde712cb412a11e02a8fb4
Daniel Sants <danielsantos619@yahoo.com.br>
2018-09-04 17:51:01 -0300

    Estrutura de repetição principal add

--- EX11 BANCO/funcoes.h
+++ EX11 BANCO/funcoes.h
@@ -1,6 +1,60 @@
 #ifndef FUNCOES_H_INCLUDED
 #define FUNCOES_H_INCLUDED
 
+//Libraries and namespaces
+#include <iostream>
+#include <locale.h>
+#include <cstring>
+#include <fstream>
+#include <conio2.h>
+using namespace std;
+//========================
 
+//Function prototypes--------------------
+bool login(char password[], char *arquivo);
+char menu(void);
+//=======================================
+
+//Structs----------------------------------
+typedef struct
+{
+    int numberCont;//Número id da conta
+    char name[21];//Nome Completo
+    double sale;//Saldo bancário
+    char password[10];//Requisitada para logar
+    bool avail;//Apagar conta lógicamente
+}stclient;
+//=========================================
+
+//Functions--------------------------------
+bool login(char password[], char *arquivo)
+{
+    ifstream fin;
+    stclient client;
+    fin.open(arquivo, ios::in);
+        fin.read((char *)(&client), sizeof(stclient));
+            while(fin && !fin.eof())
+            {
+                if(strcmp(password,client.password)==0)
+                    return true;
+                fin.read((char *)(&client), sizeof(stclient));
+            }
+        fin.close();
+        return false;
+
+}
+
+char menu(void)
+{
+    char choice;
+    cout<< "1 - Abrir conta\n"
+        << "2 - Saque\n"
+        << "3 - Depósito\n"
+        << "4 - Encerrar\n\n"
+        << "  - Digite uma opção";
+    gotoxy(1,6);
+    cin >> choice;
+    return choice;
+}
 
 #endif // FUNCOES_H_INCLUDED
--- EX11 BANCO/main.cpp
+++ EX11 BANCO/main.cpp
@@ -1,9 +1,47 @@
-#include <iostream>
+#include "funcoes.h"
 
-using namespace std;
+//QUESTAO: Considerando a seguinte descriçãa narrativa, a descrição do caso
+//de uso Realizar Saque e, utilizando os conceitos de PROGRAMAÇAO COM ARQUIVOS,
+//PONTEIROS e VETORES DINAMICOS apresentados ate o momento, implemente
+//sua soluçao.
+
+//Caso de Uso - Realizar Saque bancário
 
 int main()
-{
-    cout << "Hello world!" << endl;
+{setlocale(LC_ALL,"portuguese");
+
+    char choice, ans;
+
+    do
+    {clrscr();
+        choice = menu();
+        switch(choice)
+        {
+            case '1'://Abrir conta
+                cout << "Abrir conta";
+                cout << "\n\nOutra vez? (s/n)";
+                cin >> ans;
+            break;
+
+            case '2'://Saque
+                cout << "Saque";
+                cout << "\n\nOutra vez? (s/n)";
+                cin >> ans;
+            break;
+
+            case '3'://Depósito
+                cout << "Depositar";
+                cout << "\n\nOutra vez? (s/n)";
+                cin >> ans;
+            break;
+
+            case '4'://Encerrar
+                cout << "Tchau";
+            break;
+
+            default:
+                cout << "Erro de escolha";
+        }
+    }while(choice != '4' && toupper(ans) == 'S');
     return 0;
 }
